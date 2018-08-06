f041c2af69bcc8b82da2b1e03685890edb1058c3
Kevin <saavedra.kevin@gmail.com>
2018-08-06 16:36:57 -0700

    Update R script.

--- load_data.R
+++ load_data.R
@@ -1,6 +1,6 @@
 library(readxl)
 library(tidyverse)
-
+library(lubridate)
 
 load_bridge <- function(bridge_name) {
   bridge_data <- read_excel("data/Hawthorne Tilikum Steel daily bike counts 073118.xlsx",
@@ -16,7 +16,20 @@ total_days <- function(bridge_name) {
     return(data)
 }
 
+average_daily <- function(bridge_name) {
+  data <- bridge_name %>%
+    select(total) %>%
+    summarize(Mean = mean(total, na.rm=TRUE))
+  return(data)
+}
+
+average_monthly <- function(bridge_name) {
+  data <- bridge_name %>%
+    select(date, total) %>%
+    group_by(month(date)) %>%
+    summarize(Mean = mean(total, na.rm=TRUE))
+    return(data)
+}
+
 bridges <- c("Hawthorne", "Tilikum", "Steel")
 lapply(bridges, load_bridge)
-
-# Average Daily Bike Count
