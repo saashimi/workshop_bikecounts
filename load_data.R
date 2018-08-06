a169d79d6e1b665771d585cb224c7e88a988fd1f
Kevin <saavedra.kevin@gmail.com>
2018-08-06 15:34:02 -0700

    update r script.

---
+++ load_data.R
@@ -0,0 +1,22 @@
+library(readxl)
+library(tidyverse)
+
+
+load_bridge <- function(bridge_name) {
+  bridge_data <- read_excel("data/Hawthorne Tilikum Steel daily bike counts 073118.xlsx",
+             sheet = bridge_name, skip = 1)
+  return(bridge_data)
+}
+
+total_days <- function(bridge_name) {
+  data <- bridge_name %>%
+    select(total) %>%
+    filter(total > 0) %>%
+    tally()
+    return(data)
+}
+
+bridges <- c("Hawthorne", "Tilikum", "Steel")
+lapply(bridges, load_bridge)
+
+# Average Daily Bike Count
