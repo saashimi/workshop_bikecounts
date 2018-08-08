bd070def2e5fdd461687f40e753c8f3619201db2
Kevin <saavedra.kevin@gmail.com>
2018-08-08 13:00:26 -0700

    Working scatter plot from weather and bridge data

--- code/load_data.R
+++ code/load_data.R
@@ -5,8 +5,15 @@ library(lubridate)
 load_bridge <- function(bridge_name) {
   bridge_data <- read_excel("data/Hawthorne Tilikum Steel daily bike counts 073118.xlsx",
              sheet = bridge_name, skip = 1)
-  bridge_data <- add_column(bridge_data, name = bridge_name)
+  bridge_data <- add_column(bridge_data, name = bridge_name) %>%
+  mutate(date = as.Date(date, format = '%m/%d/%Y'))
   return(bridge_data)
+}
+
+filter_by_name <- function(bridge_name) {
+  bridge_data <- all_data %>%
+    filter(name == bridge_name)
+    return(bridge_data)
 }
 
 total_days <- function(bridge_name) {
@@ -39,7 +46,19 @@ Tilikum <-  all_bridges[2]
 Steel <- all_bridges[3]
 
 weather_data_csv <- read_csv("data/NCDC-CDO-USC00356750.csv") 
-bind_rows(Hawthorne, Tilikum, Steel)
+consolidated_bridge <- bind_rows(Hawthorne, Tilikum, Steel)
+all_data <- left_join(consolidated_bridge, weather_data_csv, by = c("date" = "DATE"))
+
+bridges <- c("Hawthorne", "Tilikum", "Steel")
+all_bridges <-  lapply(bridges, filter_by_name)
+Hawthorne <- all_bridges[[1]]
+Tilikum <-  all_bridges[[2]]
+Steel <- all_bridges[[3]]
+
+
+ggplot(Hawthorne, aes(x=PRCP, y=total)) + geom_point()
+ggplot(Tilikum, aes(x=PRCP, y=total)) + geom_point()
+ggplot(Steel, aes(x=PRCP, y=total)) + geom_point()
 
 
 #result <- c(strsplit(str, " "))
