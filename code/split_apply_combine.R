ae7fcfdf34fc6a775977e5e67bf4c553eeb4975e
Kevin <saavedra.kevin@gmail.com>
2018-08-10 11:29:43 -0700

    play with glance output.

--- code/split_apply_combine.R
+++ code/split_apply_combine.R
@@ -48,9 +48,10 @@ all_data_nested <- all_data %>%
 # Apply the linear regression model
 weather_model <- all_data_nested %>% 
   mutate(fit = purrr::map(data, tot_vs_weather),
-         tidy = purrr::map(fit, tidy)) %>% 
-  select(name, tidy) %>% 
-  unnest(tidy)
+         tidy = purrr::map(fit, tidy),
+         glance = map(fit, glance)) %>% 
+  unnest(glance)
+  #select(name, tidy) %>% 
 
 weather_model_data <- weather_model %>% 
   filter(term=="PRCP") %>% 
